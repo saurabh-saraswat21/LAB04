19024e6c71bfed73a35bc13eaa545e96d4bcf84e
Saurabh <saurabh.saraswat_cs18@gla.ac.in>
2019-09-04 16:59:29 +0530

    Student class updated

--- src/Student.java
+++ src/Student.java
@@ -1,17 +1,38 @@
 import Lab04.Person;
 
 public class Student extends Person {
+    private long UniversityRollNo;
+    private double cpi ;
 
 
-    public Student(int age, String name, char gender) {
+    public Student(int age, String name, char gender,long UniversityRollNo ,double cpi) {
         super(age, name, gender);
+        this.cpi=cpi;
+        this.UniversityRollNo=UniversityRollNo;
+    }
+    public double getCpi() {
+        return cpi;
+    }
+
+    public void setCpi(double cpi) {
+        this.cpi = cpi;
+    }
+
+    public long getUniversityRollNo() {
+        return UniversityRollNo;
+    }
+
+    public void setUniversityRollNo(long universityRollNo) {
+        UniversityRollNo = universityRollNo;
+    }
+    public void study(){
+        System.out.println("University Roll No = " + UniversityRollNo + "Cpi " + cpi   );
     }
 }
-}
 
 
 
 
-}
+
 
 
